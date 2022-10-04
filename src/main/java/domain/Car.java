0b9ad4f08b865659d149a430aa408d46a2135c6c
jang-tae-jong <jat9276@naver.com>
2022-10-05 01:23:03 +0900

    feat : 예외처리 추가

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -1,5 +1,7 @@
 package domain;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 
 public class Car {
@@ -8,17 +10,40 @@ public class Car {
 
     public Car(String[] cars) {
         checkCarName(cars);
+        checkCarCount(cars);
         this.cars = cars;
+    }
+
+
+    private void checkCarCount(String[] cars) {
+        if(cars.length==0){
+            throw new IllegalArgumentException("[ERROR] 1개 이상의 이름을 입력하여야 합니다.");
+        }
     }
 
     private void checkCarName(String[] cars) {
         for (String carName : cars){
-            checkNameLength(carName);
+            checkNameMaxLength(carName);
+            checkNameMinLength(carName);
+            checkNameUnique(carName,cars);
         }
     }
 
-    private void checkNameLength(String carName) {
-        if(carName.length()>5 ||carName.length()==0){
+    private void checkNameMinLength(String carName) {
+        if(carName.length()==0){
+            throw new IllegalArgumentException("[ERROR] 이름은 1글자 이상이여야합니다.");
+        }
+    }
+
+    private void checkNameUnique(String carName,String[] cars) {
+        if(Collections.frequency(Arrays.asList(cars),carName)>1){
+            throw new IllegalArgumentException("[ERROR] 중복된 이름은 입력할 수 없습니다.");
+        }
+    }
+
+    private void checkNameMaxLength(String carName) {
+
+        if(carName.length()>5){
             throw new IllegalArgumentException("[ERROR] 이름은 5글자 이하여야합니다.");
         }
     }
